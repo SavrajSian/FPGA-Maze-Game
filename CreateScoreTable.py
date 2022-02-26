0ced5eb79639ea1d441f2d54f0491fa428da5e70
mahasch <mahasch140@gmail.com>
2022-02-26 18:17:13 +0000

    creating dynamic score table

---
+++ CreateScoreTable.py
@@ -0,0 +1,40 @@
+import boto3
+
+def create_score_table(dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
+
+    table = dynamodb.create_table(
+        TableName='Scores',
+        KeySchema=[
+            {
+                'AttributeName': 'player',
+                'KeyType': 'HASH'  # Partition key
+            },
+            {
+                'AttributeName': 'score',
+                'KeyType': 'RANGE'  # Sort key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'player',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'score',
+                'AttributeType': 'N'
+            },
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 100,
+            'WriteCapacityUnits': 100
+        }
+    )
+    return table
+
+
+if __name__ == '__main__':
+    score_table = create_score_table()
+    print("Table status:", score_table.table_status)
